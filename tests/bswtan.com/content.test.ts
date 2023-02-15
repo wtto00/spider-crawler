b857bdd2c05b330c3f062f432bb370510a7e56cf
wtto <wtto00@outlook.com>
2023-02-15 15:40:21 +0800

    fix #35

--- tests/bswtan.com/content.test.ts
+++ tests/bswtan.com/content.test.ts
@@ -25,7 +25,7 @@ const options: CrawlerUrlOptions = {
 test('book content', () => {
   return crawlFromUrl(options).then((res) => {
     expect(res.code).toBe(ResultCodes.SUCCESS);
-    expect(res.data['title']).toEqual('1、想等的人');
+    expect(res.data['title']).toEqual('1、想等的人（修）');
     expect(res.data['previousChapter']).toEqual('https://www.bswtan.com/7/7657/');
     expect(res.data['nextChapter']).toEqual('https://www.bswtan.com/7/7657/4846089.html');
     expect(res.data['content'].length).toBeGreaterThan(0);
